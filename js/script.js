7c6e4e2508b0995b0a038e929c6a10cddf0b0055
Mayank Sharma <msharma@iitk.ac.in>
2017-12-09 00:42:26 +0530

    Added all the basic components

---
+++ js/script.js
@@ -0,0 +1,45 @@
+ $(document).ready(function(){
+              $('#btn1').hover(function(){
+                 $('#c1').toggle(300);});
+              
+			  $('#btn2').hover(function(){
+                 $('#c2').toggle(300);});
+              
+			  $('#btn3').hover(function(){
+                 $('#c3').toggle(300);});
+              
+			  $('#btn4').hover(function(){
+                 $('#c4').toggle(300);});
+              
+			  $('#btn5').hover(function(){
+                 $('#c5').toggle(300);});
+              
+			  $('#btn6').hover(function(){
+                 $('#c6').toggle(300);});
+              
+			  $('#btn7').hover(function(){
+                 $('#c7').toggle(300);});
+              
+			  $('#btn8').hover(function(){
+                 $('#c8').toggle(300);});
+              
+			  $('#btn9').hover(function(){
+                 $('#c9').toggle(300);});
+              
+			  $('#btn10').hover(function(){
+                 $('#c10').toggle(300);});
+              
+			  $('#btn11').hover(function(){
+                 $('#c11').toggle(300);});
+              
+			  $('#btn12').hover(function(){
+                 $('#c12').toggle(300);});
+              
+			  $('#btn13').hover(function(){
+                 $('#c13').toggle(300);});
+              
+			  $('#btn14').hover(function(){
+                 $('#c14').toggle(300);});
+			  $('#btn15').hover(function(){
+                 $('#c15').toggle(300);});
+              })
